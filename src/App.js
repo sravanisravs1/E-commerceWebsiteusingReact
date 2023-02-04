a518a36607f4ea29f1ef17372a2c37c5993e9de3
sravanisravs1 <kodurusravani813@gmail.com>
2023-02-04 19:36:06 +0530

    Added Movies through API calls

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { useState ,useContext, Fragment} from "react";
 import About from './pages/About';
 import Home from "./pages/Home";
 import { createBrowserRouter ,RouterProvider} from "react-router-dom";
-import Movies from "./components/APICalls/Movies";
-
+import MovieList from './components/Movies/MovieList'
 
 const router = createBrowserRouter([
   
@@ -13,6 +12,7 @@ const router = createBrowserRouter([
 ]);
 
 const  App = (props) => {
+  const [movies,setMovies ] = useState([]);
 //   function MoviesList () {
 //     fetch('https://swapi.dev/api/films/')
 //     .then((response)=>{ return response.json();
@@ -23,14 +23,27 @@ const  App = (props) => {
   async function MoviesList () {
       const response= await fetch('https://swapi.dev/api/films/');
       const data = await response.json();
-      const results= data.results
-      return (console.log(results))
+      const transformedMovies = data.results.map((movieData=>{
+        return ({
+          id:movieData.episode_id,
+          title:movieData.title,
+          releaseDate:movieData.release_date,
+          openingText:movieData.opening_crawl
+        });
+
+      }));
       
+    setMovies(transformedMovies)  
   }
   return(
   <Fragment>
       <RouterProvider router={router}/>
-      <Movies MoviesList={MoviesList}/>
+      <section>
+          <button onClick={MoviesList}>
+            FetchMovies
+          </button>
+      </section>
+      <MovieList movies={movies}/>
   </Fragment>
     
   )}
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Movie = (props) => {
+  return (
+    <li >
+      <h2>{props.title}</h2>
+      <h3>{props.releaseDate}</h3>
+      <p>{props.openingText}</p>
+    </li>
+  );
+};
+
+export default Movie;
