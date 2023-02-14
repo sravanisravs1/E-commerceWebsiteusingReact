d976e14a778eae986eb50225b058c9aa7c49757a
sravanisravs1 <kodurusravani813@gmail.com>
2023-02-14 09:59:41 +0530

    lazy loading and suspense

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React ,{ lazy, Suspense }from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Header from "./components/Layout/Header";
-import Products from './Pages/Products'
-import CartProvider from "./store/CartProvider";
+// import Header from "./components/Layout/Header";
+// import Products from './Pages/Products'
+ import CartProvider from "./store/CartProvider";
 
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import ContactUs from './Pages/ContactUs';
-import SingleProduct from "./Pages/SingleProduct";
-import Login from "./components/Auth/Login";
+// import Home from "./Pages/Home";
+// import About from "./Pages/About";
+// import ContactUs from './Pages/ContactUs';
+// import SingleProduct from "./Pages/SingleProduct";
+// import Login from "./components/Auth/Login";
+const Header = lazy(() => import("./components/Layout/Header"));
+const Home = lazy(() => import("./Pages/Home"));
+const Products = lazy(() => import("./Pages/Products"));
+const SingleProduct = lazy(() => import("./Pages/SingleProduct"));
+const About = lazy(() => import("./Pages/About"));
+const ContactUs = lazy(() => import('./Pages/ContactUs'));
+const Login = lazy(() => import("./components/Auth/Login"));
 
 
 const router = createBrowserRouter( [ 
@@ -17,13 +24,13 @@ const router = createBrowserRouter( [
     path: '/',
     element: <Header/>,
     children: [
-      { path: '/', element: <Home/> },
-      { path: '/products', element: <Products/> },
-      { path: '/about', element: <About/> },
-      { path: '/contact', element: <ContactUs/>},
-      {path: '/products/:id', element: <SingleProduct/>},
-      {path: '/login', element: <Login/>},
-      {path:'*', element:<Login/>}
+      { path: '/', element: <Suspense><Home/></Suspense> },
+      { path: '/products', element: <Suspense><Products/></Suspense> },
+      { path: '/about', element: <Suspense><About/></Suspense> },
+      { path: '/contact', element: <Suspense><ContactUs/></Suspense>},
+      {path: '/products/:id', element: <Suspense><SingleProduct/></Suspense>},
+      {path: '/login', element: <Suspense><Login/></Suspense>},
+      {path:'*', element:<Suspense><Login/></Suspense>}
       // {path:'/logout',element:<Logout/>}
     ],
   },
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,91 +1,10 @@
-import React, { useState,useReducer ,useEffect} from "react";
+import React, { useState,useEffect} from "react";
 import axios from "axios";
 
 import CartContext from './cart-context';
 
 
 
-// const defaultState = {
-//     products:[],
-//     totalAmount: 0,
-//     products: [] 
-   
-// }
-
-// const cartReducer = (state, action) => {
-//     if (action.type === "ADD") {
-//         const updatedAmount = state.totalAmount + action.value.price * action.value.quantity;
-
-
-//         const existsItemIndex = state.products.findIndex((index) => index.id === action.value.id)
-//         const existsItem = state.products[existsItemIndex]
-
-
-
-//         let update = state.products.concat(action.value)
-//         if (existsItem) {
-//             alert("item alreadry exisits")
-//             return {
-//                 products:state.products,
-//                 totalAmount:state.totalAmount
-//             }
-
-//         }
-
-//     // to add the data in localStorage
-//   // get vs set
-
-  
-//         return {
-//             products: update,
-//             totalAmount: updatedAmount
-//         }
-//     }else if (action.type === "REMOVE"){
-//         const updatedAmount = state.totalAmount - action.value.price * action.value.quantity;
-//       const updatedProducts = state.products.filter(product => product.id !== action.value.id)
-
-//       return {
-//                    products: updatedProducts,
-//                     totalAmount: updatedAmount
-//                }
-//     }
-
-    
-//     return defaultState
-// }
-
-
-// const CartProvider = (props) => {
-    // const [state, dispatchFN] = useReducer(cartReducer, defaultState)
-
-
-    // const addProductHandler = (product) => {
-    //     dispatchFN({
-    //         type: "ADD",
-    //         value: product
-
-    //     })
-       
-    // }
-
-    
-
-    // const removeProductHandler = (product) => { 
-    //     dispatchFN({
-    //         type:"REMOVE",
-    //         value:product
-    //     })
-    // }
-
-
-    // const Cart = {
-    //     products: state.products,
-    //     totalAmount: state.totalAmount,
-    //     addItem: addProductHandler,
-    //     removeItem: removeProductHandler
-
-
-    // }
     const CartProvider = (props) => {
         const [Item, setCartItems] = useState([]);
       
